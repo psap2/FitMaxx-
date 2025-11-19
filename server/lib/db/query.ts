587c9b86e49144d62263f1d80ab8b39154ecf3b6
Dan Shallal <dshallal11@gmail.com>
2025-11-19 17:09:47 -0500

    changes

--- server/lib/db/query.ts
+++ server/lib/db/query.ts
@@ -1,4 +1,4 @@
-import { supabase } from "../supabase";
+import { supabase } from "../../../utils/supabase";
 import { Post, User } from "./schema";
 
 export const getUser = async (email: string | undefined) => {
